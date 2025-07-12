8a9b5f59e915e48d6c87c0aea467ca1037bfb784
ShadowShogun00 <nitinpawar3081@gmail.com>
2025-07-12 12:25:31 +0530

    Add files via upload
    first files

---
+++ script.js
@@ -0,0 +1,119 @@
+let totalexpense =0;
+
+
+const modeButton = document.querySelector('.change-mode');
+const modeIcon = modeButton.querySelector('i');
+
+let isDarkMode = false;
+
+modeButton.addEventListener('click', () => {
+
+    document.body.classList.toggle('dark-mode');
+
+    if (!isDarkMode) {
+        modeIcon.classList.remove('fa-moon');
+        modeIcon.classList.add('fa-sun');
+        modeButton.style.backgroundColor = '#333';
+    } else {
+        modeIcon.classList.remove('fa-sun');
+        modeIcon.classList.add('fa-moon');
+        modeButton.style.backgroundColor = 'red'; 
+    }
+
+    isDarkMode = !isDarkMode;
+});
+
+const expensebtn = document.getElementById('addexpensebtn');
+const expenseContainer = document.getElementById('expense-container');
+const cbtn = document.getElementById('cancelbtn');
+const sbtn = document.getElementById('submitbtn');
+
+expensebtn.addEventListener('click', () => {
+    expenseContainer.style.display = 'block';
+    cbtn.style.display ='block';
+    sbtn.style.display = 'block';
+    expensebtn.style.display = 'none';
+});
+
+cbtn.addEventListener('click', () => {
+    expenseContainer.style.display = 'none';
+    sbtn.style.display = 'none';
+    cbtn.style.display = 'none';
+    expensebtn.style.display = 'block';
+});
+
+    sbtn.addEventListener('click', () => {
+    const tbody = document.getElementById('expense-body');
+    const date = document.getElementById('datebox').value;
+    const categorySelect = document.getElementById('categorybox');
+    const category = categorySelect.value;
+    const categoryText = categorySelect.options[categorySelect.selectedIndex].text;
+    const item = document.getElementById('itembox').value;
+    const amount = document.getElementById('amountbox').value;
+
+    if (date === '') {
+        alert('Please enter a date');
+        return;
+    }
+    if (category === '0') {
+        alert('Please select a category');
+        return;
+    }
+    if (item === '') {
+        alert('Please enter an item');
+        return;
+    }
+    if (amount === '') {
+        alert('Please enter an amount');
+        return;
+    }
+
+    const newRow = document.createElement('tr');
+
+    const dateCell = document.createElement('td');
+    dateCell.innerText = date;
+
+    const categoryCell = document.createElement('td');
+    categoryCell.innerText = categoryText;
+
+    const itemCell = document.createElement('td');
+    itemCell.innerText = item;
+
+    const amountCell = document.createElement('td');
+    amountCell.innerText = amount;
+
+    newRow.appendChild(dateCell);
+    newRow.appendChild(categoryCell);
+    newRow.appendChild(itemCell);
+    newRow.appendChild(amountCell);
+
+    tbody.appendChild(newRow);
+
+    document.getElementById('date').value = '';
+    categorySelect.value ='0';
+    document.getElementById('item').value = '';
+    document.getElementById('amount').value = '';
+    
+    expenseContainer.style.display = 'none';
+    sbtn.style.display = 'none';
+    cbtn.style.display = 'none';
+    expensebtn.style.display = 'block';
+
+    const balancecell = document.getElementById('rbalance');
+    const expensecell = document.getElementById('texpense');
+    const income = document.getElementById('income').value;
+    const cincome = parseFloat(income);
+
+    const rows = tbody.rows;
+    let total = 0;
+    for(let i = 0; i< rows.length; i++){
+        const amountCell = rows[i].cells[3];
+        const amount = parseFloat(amountCell.innerText);
+        total += amount;
+    }
+    expensecell.innerText = total;
+    let bcell;
+    bcell = cincome - total;
+    balancecell.innerText = bcell;
+
+    });
